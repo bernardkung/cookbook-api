1490b9fc5656b57203904ba0b5ed62815010b8b0
bernardkung <bpkung@gmail.com>
2021-12-24 18:13:54 -0500

    Basic recipe adding finished

--- routes/recipes.js
+++ routes/recipes.js
@@ -2,21 +2,59 @@ const express = require('express');
 const router = express.Router();
 const fs = require('fs')
 
-/* GET users listing. */
-router.get('/', (req, res, next)=>{
-  const recipesFiles = fs.readdirSync('./public/recipes')
+
+function getRecipes() {
+  const recipesFiles = fs.readdirSync('./public/recipes', err=>{
+    if (err) throw err;
+  })
   const recipes = recipesFiles.map(filename => {
-    const file = fs.readFileSync("./public/recipes/" + filename)
+    const file = fs.readFileSync("./public/recipes/" + filename, err=>{
+      if (err) throw err;
+    })
     const recipe = JSON.parse(file)
     return recipe
   })
+  return recipes
+}
 
-  res.json({"recipes":recipes});
+async function saveRecipe(recipe){
+  const filepath = './public/recipes/'
+  const filename =  cleanString(recipe.name) + ".json"
+  await fs.promises.writeFile(filepath + filename, JSON.stringify(recipe), { flag: 'wx' }, (err)=>{
+    if (err) throw err;
+  })
+}
+
+function cleanString(inStr){
+  const resChars = ['<', '>', ':', '"', '/', '\\', '|', '?', '*', '.']
+  const outStr = inStr.replace(/[<>:"/\\|?*.]/g, '');
+  return outStr.toLowerCase()
+}
+
+/* GET users listing. */
+router.get('/', (req, res, next)=>{
+  const recipes = getRecipes()
+  res.json({recipes})
 });
 
 router.post('/', (req, res)=>{
-  console.log("api receive:", req.body)
-  res.sendStatus(200)
+  console.log("Receiving:", req.body.recipe)
+  const recipe = req.body.recipe
+  // Save recipe as a JSON
+  saveRecipe(recipe)
+    .then(()=>{
+      try {
+        console.log("getting recipes")
+        const recipes = getRecipes()
+        console.log(recipes)
+        return res.status(200).json({recipes})
+      } catch(err) {
+        console.warn(err)
+        return res.status(400).json({status:"failed to refresh recipes"})
+      }
+    })
+    .catch(err=>res.status(406).json({status:err}))
+    
 })
 
 module.exports = router;
